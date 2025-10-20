e54f1edb8491731c13352e3e1fa6442084cd2509
Lam Huu Tho <thopk135@gmail.com>
2025-10-20 22:04:45 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ app.use('/api/payment', paymentRoutes);
 app.use('/api/analytics', analyticsRoutes);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "/frontend/dist"))); 
+    app.use(express.static(path.join(__dirname, "../frontend/dist"))); 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
     });
 }
 
